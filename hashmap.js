4441b5ad6429a106f8227c8257edcc292cf478d3
unknown <140576667+Dungnope@users.noreply.github.com>
2026-01-17 18:42:19 +0700

    create hashmap class and set method, use linkedlist for collision with chaining technique

---
+++ hashmap.js
@@ -0,0 +1,68 @@
+import { LinkedList, Node } from "./linkedlist.js";
+
+class HashMap{
+    constructor(capacity, load_factor){
+        this.load_factor = load_factor === undefined ? 0.75 : load_factor;
+        this.capacity = capacity === undefined ? 10 : capacity;
+        this.container = Array(this.capacity).fill(null);
+    }
+
+    hash(key){
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        if(typeof(key) === "number"){
+            for (let i = 0; i < key; i++) {
+                hashCode = (primeNumber * hashCode + key) % this.capacity;
+            }
+
+        }
+        else if(typeof(key) === 'string'){
+            // s[0]*31^(n-1) + s[1]*31^(n-2) + ... + s[n-1]
+            for(let i = 0; i < key.length; i++){
+                hashCode += (key.charCodeAt(i)*31**(key.length - (i+1))); // take from java ulti libs
+            }
+            hashCode %= this.capacity;
+        }
+
+        return hashCode;
+    }
+
+    set(key, value){
+        let index = this.hash(key);
+        
+        if(this.container[index] === null){
+            this.container[index] = new LinkedList(value, key);
+        }
+
+        else if(this.container[index] !== null){
+
+            //resolve collision
+            if(this.container[index].root.key === key){
+                const traverseNode = this.container[index].root
+                while(traverseNode.key !== key){
+                    traverseNode = traverseNode.next;
+                }
+                traverseNode.data = value;
+            }
+            else{
+                this.container[index].append(value, key);
+            }
+        }
+    }
+
+}
+
+const test = new HashMap(10, 0.75);
+
+test.set("100", 23);
+test.set("231", 45);
+test.set("123", 69);
+test.container.forEach((item, idx) => {
+    if(idx === 0){
+        console.log("[");
+    }
+    console.log(item, idx);
+
+    if(idx === test.length - 1) console.log("]");
+})
---
+++ linkedlist.js
@@ -0,0 +1,231 @@
+class Node{
+    constructor(data, key){
+        this.data = data === undefined ? null : data;
+        this.key = key;
+        this.next = null;
+    }
+}
+
+
+class LinkedList{
+    constructor(value, key){
+        this.root = new Node(value, key);
+    }
+
+    //check blank list
+    checkBlank(){
+        return this.root.data === null ? true : false;
+    }
+    
+    //add new node at the end of list
+    append(data, key){
+        if(this.checkBlank()){
+            this.root = new Node(data, key);
+        }
+        else{
+            let tmp = this.root;
+            while(tmp.next !== null) tmp = tmp.next; 
+
+            tmp.next = new Node(data, key);
+        }
+    }
+
+    //add new node at start of list
+    prepend(data){
+        if(this.checkBlank()){
+            this.root = new Node(data);
+        }
+        else{
+            let tmp = this.root;
+            this.root = new Node(data);
+            this.root.next = tmp;
+        } 
+    }
+
+    //number node of list
+    size(){
+        let number = 0;
+        let tmp = this.root;
+        while(tmp !== null) 
+        {
+            number++
+            tmp = tmp.next;
+        }
+        return number;
+    }
+
+    //the value of first node
+    head(){
+
+        if(this.checkBlank()) return undefined;
+
+        return this.root.next.data;
+        
+    }
+
+    //the value of last node
+    tail(){
+        if(this.checkBlank()) return undefined;
+
+        let tmp = this.root;
+        while(tmp.next !== null) tmp = tmp.next;
+        return tmp.data;
+    }
+
+    //return data at index input
+    at(index){ 
+        let tmp = this.root;
+
+        if(this.size() - 1 < index) return undefined;
+
+        while(index && tmp !== null){
+            tmp = tmp.next;
+            index--;
+        }
+        
+        return tmp !== null ? tmp.data : undefined;
+    }
+
+    //remove head node and return its value
+    pop(){
+        if(this.checkBlank()) return undefined;
+
+        let ans = this.root;
+        this.root = this.root.next;
+        ans.next = null;
+        return ans;
+    }
+
+    //check if value in list or not
+    contains(value){
+        let tmp = this.root;
+
+        while(tmp !== null){
+            if(tmp.data === value) return true;
+            tmp = tmp.next;
+        }
+        return false;
+    }
+
+    //return index of value if exist
+    findIndex(value){
+        if(this.checkBlank()) return -1;
+        let index = 0;
+        let tmp = this.root;
+        while(tmp !== null){
+            if(tmp.data === value) return index;
+            tmp = tmp.next;
+            index++;
+        }
+
+        return -1;
+    }
+
+    toString(){
+        if(this.checkBlank()) return `( ) -> `;
+        let ans = "";
+        let tmp = this.root;
+        while(tmp !== null){
+            ans += `( ${tmp.data} )`;
+            ans += " -> ";
+            tmp = tmp.next;
+        }
+        ans += tmp;
+        return ans;
+    }
+
+    //insert elements at specific position
+    insertAt(index, ...value){
+
+        //check whether index in head or tail
+        if(index === 0){
+            for (const item of value.reverse()) {
+                this.prepend(item);
+            }
+            return;
+        }
+        else if(index === this.size() - 1){
+            for (const item of value) {
+                this.append(item);
+            }
+            return;
+        }
+
+        //check if index out of bound
+        if(index > this.size() - 1 || index < 0) return `RangeError`;
+
+        // make array become linkedlist
+        function convertToList(array){
+            let list_from_array = new LinkedList();
+            for(let i = 0; i < array.length; i++){
+                list_from_array.append(array[i]);
+            }
+
+            return list_from_array;
+        }
+
+
+        //create new list from array
+        let array_list = convertToList(value);
+
+
+        let tmp_head = this.root;
+        while(index > 1){
+            tmp_head = tmp_head.next;
+            index--;
+        }
+
+        //make a node that point to next node of tmp_head
+        let tmp_catch_tail = tmp_head.next;
+
+        // make tmp_head node point to array_list
+        tmp_head.next = array_list.root;
+
+        // tmp_tail point to first node of array_list
+        let tmp_tail = tmp_head;
+        while(tmp_tail.next !== null) tmp_tail = tmp_tail.next;
+        tmp_tail.next = tmp_catch_tail;
+    }
+    
+    removeAt(index){
+        if(index > this.size() - 1 || index < 0) return `RangeError`;
+
+        //check whether index in head or tail
+        if(index === 0){
+            this.pop();
+            return;
+        }
+        else if(index === this.size() - 1){
+            let tmp = this.root;
+            while(tmp.next.next !== null) tmp = tmp.next;
+            tmp.next = null;
+            return;
+        }
+
+        let tmp = this.root;
+        while(index > 1){
+            tmp = tmp.next;
+            index--;
+        }
+        tmp.next = tmp.next.next;
+    }
+}
+
+// const test = new LinkedList();
+
+// //add node to the end of list test
+// test.append("dog");
+// test.append("cat");
+// test.append("parrot");
+// test.append("hamster");
+// test.append("snake");
+// test.append("turtle");
+// test.insertAt(2, "mouse");
+
+// //test convert linkedlist to string
+// console.log(test.toString());
+
+// test.removeAt(3); // test remove at position
+// console.log(test.toString());
+
+export {LinkedList, Node};
